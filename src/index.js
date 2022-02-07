e6fc2d83b5530c9d109b9093fbac00cc4237ba84
seunghwan <hon6036@gmail.com>
2022-02-07 21:38:52 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import express from 'express'
+import { sphinxDBconnection } from './dbConnection.js'
+import mysql from 'mysql'
+
+var app = express()
+
+// input: image of item, stats, public key, name of game
+app.post('/mintGameNFT', async(req, res) => {
+})
+
+// input: public key, name of game
+app.post('/getItemInfo', async(req, res) => {
+
+})
+
+// input: address of old image, stats, address of new image
+app.post('/changeItmeImage', async(req, res) => {
+
+})
+
+// input: address of image
+app.get('/changeGame', async(req, res) => {
+    const image = 123
+    const stat = 23
+    const game = "maple"
+    const insert = mysql.format('insert into nft_binding_list (image, stat, game) values (?, ?, ?)', [image, stat, game]);
+    console.log(123123)
+    const conne = await sphinxDBconnection.getConnection(function(err, conn) {
+        console.log(err)
+        conn.query(insert, function(error, data) {
+            if (error) {
+                console.log(error)
+            }
+            console.log(data)
+        })
+        conn.release()
+    })
+    console.log(conne)
+})
+
+// input: image, value of image, public key
+app.post('/mintDesignNFT', async(req, res) => {
+
+})
+
+app.listen(3030, () => {
+    console.log(`Example app listening on port ${3030}`)
+  })

ef47ab33de329afa442b2b9d5a23a5ecc2897038
Hyeojin_Jhon <hyojin5658@gmail.com>
2022-02-11 19:46:56 +0900

    create new token.sol file
    make an erc20 tokens for sphinx. 
    but it doens't work  buy token.
    Becuase  token owner have to doing approve tokens to spender.

---
+++ src/contracts/token.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+
+
+contract MyToken is ERC20 {
+    uint256 public constant tokenpereth = 5;  // 1 ether = token 5개 
+    address public tokenOwner;
+
+    event Buy(address tokenowner, uint256 balance_owner, uint256 amount);
+
+    constructor() ERC20("SphinxToken", "churr") {
+        // Mint 100 tokens to msg.sender
+        // Similar to how
+        // 1 dollar = 100 cents
+        // 1 token = 1 * (10 ** decimals)
+        // _mint(msg.sender, 100 * 10**uint(decimals()));
+        tokenOwner = msg.sender;
+        _mint(msg.sender, 100*10**decimals());
+    }
+
+    function getTokenOwner() view public returns (address){ 
+        return tokenOwner;
+    }
+
+    function buytoken(uint256 amount) public {
+        emit Buy(tokenOwner, balanceOf(tokenOwner), amount);
+        require(balanceOf(tokenOwner) >=  amount, "it is too much to tranasfer");
+        // approve(tokenOwner,amount);
+        // increaseAllowance(tokenOwner,amount);
+        transferFrom( tokenOwner, msg.sender, amount);
+    }
+
+    
+
+    // function buy() external payable returns (address, uint256, uint256){
+    //     uint256 amount = msg.value * tokenpereth;
+    //     emit Buy(tokenOwner, balanceOf(tokenOwner), amount);
+
+    //     require(balanceOf(tokenOwner) >=  amount, "it is too much to tranasfer");
+    //     approve(tokenOwner,amount);
+    //     increaseAllowance(tokenOwner,amount);
+    //     transferFrom(tokenOwner, msg.sender, amount);
+    //     return (tokenOwner,balanceOf(tokenOwner),amount);
+
+    // }  
+
+}

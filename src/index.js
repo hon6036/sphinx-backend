8a4cf3ae1b8eb610e36dbc32ae02e3f611d1b403
Cheol_Hoon <cch726@hanyang.ac.kr>
2022-02-17 16:53:06 +0900

    ipfs nft upload version: 0.1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,105 @@
 import express from 'express'
 import { sphinxDBconnection, game1DBconnection, game2DBconnection } from './dbConnection.js'
 import mysql from 'mysql'
+import { create } from 'ipfs-http-client';
 
-var app = express()
+const ipfs = create();
+const app = express();
 
-// input: image url, stats, public key, name of game
+// input: image url, stats, public key, name of game - cheolhoon
 app.post('/mintGameNFT', async(req, res) => {
+    var img_hash = '';
+    var stat_hash = '';
+    var attr_img_hash = '';
+    var attr_stat_hash = '';
+    var attr_img = {
+        issuer: 'Sphinx',
+        game: req.body.game,
+        hash: '',
+    };
+    var attr_stat = {
+        issuer: 'Sphinx',
+        game: req.body.game,
+        hash: '',
+    }
+    //assume image exist as buffer
+    //img save at ipfs
+    await ipfs.add(req.body.img)
+    .then((response) => {
+        console.log(response);
+        img_hash = response.path;
+        attr_img.hash = response.path;
+    });
+    //img attribute save at ipfs
+    await ipfs.add(Buffer.from(JSON.stringify(attr_img)))
+    .then((response) => {
+        console.log(response);
+        attr_img_hash = response.path;
+    });
+    //stat save at ipfs
+    await ipfs.add(req.body.stat)
+    .then((response) => {
+        console.log(response);
+        stat_hash = response.path;
+        attr_stat.hash = response.path;
+    });
+    //stat attribute save at ipfs
+    await ipfs.add(Buffer.from(JSON.stringify(attr_stat)))
+    .then((response) => {
+        console.log(response);
+        attr_stat_hash = response.path;
+    });
+
+    //save img_hash, stat_hash at sphinx db
+    const mintGameNFT = mysql.format('insert into nft_binding_list(image, stat, game) values(?, ?, ?);', [img_hash, stat_hash, req.body.game]);
+    const conne = await sphinxDBconnection.getConnection(function(err, conn) {
+        console.log(err);
+        conn.query(mintGameNFT, function(error, data) {
+            if (error) {
+                console.log(error);
+            }
+            res.send({
+                img_hash: img_hash,
+                stat_hash: stat_hash,
+                attr_img_hash: attr_img_hash,
+                attr_stat_hash: attr_stat_hash
+            })
+        })
+        conn.release();
+    })
+    console.log(conne);
+})
+
+// Txhash store at sphinx db, input: img_tx_hash, stat_tx_hash, img_hash - cheolhoon
+app.post('/saveTxHash', async(req, res) => {
+    const saveTxHash = mysql.format('update nft_binding_list set img_tx_hash = ?, stat_tx_hash = ? where image = ?', [req.body.img_tx_hash, req.body.stat_tx_hash, req.body.img_hash]);
+    const conne = await sphinxDBconnection.getConnection(function(err, conn) {
+        console.log(err);
+        conn.query(saveTxHash, function(error, data) {
+            if (error) {
+                console.log(error);
+            }
+            res.send('success');
+        });
+        conn.release();
+    });
+    console.log(conne);
+});
+
+// User confirmed transaction, input: img_tx_hash - cheolhoon
+app.get('/confirmTx', async(req, res) => {
+    const confirmTx = mysql.format('update nft_binding_list set confirm_status = 1 where img_tx_hash = ?;', [req.query.img_tx_hash]);
+    const conne = await sphinxDBconnection.getConnection(function(err, conn) { 
+        console.log(err);
+        conn.query(confirmTx, function(error, data) {
+            if (error) {
+                console.log(error);
+            }
+            res.send('success');
+        });
+        conn.release();
+    });
+    console.log(conne);
 })
 
 // input: public key, name of game
@@ -64,9 +158,46 @@ app.get('/changeItemGame', async(req, res) => {
     console.log(conne)
 })
 
-// input: image, value of image, public key
+// input: image, value of image, public key - cheolhoon
 app.post('/mintDesignNFT', async(req, res) => {
+    var img_hash = '';
+    var attr_img_hash = '';
+    var attr_img = {
+        issuer: 'Sphinx',
+        game: req.body.game,
+        hash: '',
+    };
+    //assume image exist as buffer
+    //img save at ipfs
+    await ipfs.add(req.body.img)
+    .then((response) => {
+        console.log(response);
+        img_hash = response.path;
+        attr_img.hash = response.path;
+    });
+    //img attribute save at ipfs
+    await ipfs.add(Buffer.from(JSON.stringify(attr_img)))
+    .then((response) => {
+        console.log(response);
+        attr_img_hash = response.path;
+    });
 
+    //save img_hash at sphinx db
+    const mintDesignNFT = mysql.format('insert into nft_binding_list(image) values(?);', [img_hash]);
+    const conne = await sphinxDBconnection.getConnection(function(err, conn) {
+        console.log(err);
+        conn.query(mintDesignNFT, function(error, data) {
+            if (error) {
+                console.log(error);
+            }
+            res.send({
+                img_hash: img_hash,
+                attr_img_hash: attr_img_hash,
+            })
+        })
+        conn.release();
+    })
+    console.log(conne);
 })
 
 app.listen(3030, () => {
